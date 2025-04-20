1e91129b572e2e09c518d10df0812044ac6808e4
Atanael Mendes <atanaelmendes@gmail.com>
2025-04-20 16:10:38 -0300

    WIP savepont

---
+++ lib/components/transactionList.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Transactionlist extends StatelessWidget {
+  const Transactionlist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/components/transationForm.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Transationform extends StatelessWidget {
+  const Transationform({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
